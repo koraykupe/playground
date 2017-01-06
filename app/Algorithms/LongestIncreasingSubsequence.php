b705469f0e89fcfa1bef1084bd76e2567b834c5e
Koray <koraykupe@gmail.com>
2017-01-06 14:13:15 +0300

    Add LIS Algorithm with 2 solutions

--- app/Algorithms/LongestIncreasingSubsequence.php
+++ app/Algorithms/LongestIncreasingSubsequence.php
@@ -8,18 +8,21 @@
  * longest increasing subsequence number
  */
 
-$nums = [9, 1, 3, 7, 5, 6, 20];
-
+// $nums = [6, 1, 2, 2, 9, 6, 7];
+$nums = [10, 22, 9, 33, 21, 50, 41, 60];
 function lengthOfLIS($nums) {
     $list = [];
 	foreach ($nums as $num) {
+        // If it is first number or bigger than last number of LIS array add it to the array
         if(count($list) == 0 || $num > end($list)) {
             array_push($list, $num);
         } else {
             $i=0;
             $j=count($list)-1;
 
+            // Binary search
             while($i<$j) {
+                // Find middle 'pivot' number of the LIS array
                 $mid = ($i+$j)/2;
                 if($list[$mid] < $num) {
                     $i=$mid+1;
@@ -28,8 +31,26 @@ function lengthOfLIS($nums) {
             $list[$j] = $num;
         }
     }
-
 	return count($list);
 }
 
-echo lengthOfLIS($nums);
+// Naive Solution
+function lengthOfLIS2($nums) {
+    $list = [];
+    foreach ($nums as $num) {
+        if(count($list) == 0) {
+            $list[0] = $num;
+        } else {
+            if ($num > end($list)) {
+                array_push($list, $num);
+            } else {
+                $list[count($list)-1] = $num;
+            }
+        }
+    }
+    return count($list);
+}
+
+// echo lengthOfLIS($nums);
+echo "<br />";
+echo lengthOfLIS2($nums);
